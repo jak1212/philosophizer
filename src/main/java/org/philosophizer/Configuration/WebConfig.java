75e695f11fa6145611c2b13f9a814d1f7444e926
jak1212 <jk0827@gmail.com>
2026-04-21 09:07:21 -0400

    adding webconfig handling public path for static resources and CORS control for audience API

---
+++ src/main/java/org/philosophizer/Configuration/WebConfig.java
@@ -0,0 +1,23 @@
+package org.philosophizer.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/public/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://app.startsmallstarttoday.com")
+                .allowedMethods("POST")
+                .allowedHeaders("*");
+    }
+}
