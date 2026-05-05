21cb22c6b226a1159ca356bd737992eb0973b640
jak1212 <jk0827@gmail.com>
2026-05-05 08:50:36 -0400

    collapsing flyway V3 files to single file

--- src/main/resources/db/migration/V3__seed_audience.sql
+++ src/main/resources/db/migration/V3__seed_audience.sql
@@ -1,3 +1,6 @@
+CREATE SEQUENCE IF NOT EXISTS audience_seq START 1 INCREMENT 50;
+CREATE SEQUENCE IF NOT EXISTS philosophy_seq START 1 INCREMENT 50;
+
 INSERT INTO audience (first_name, last_name, email, active)
 VALUES ('Jack', 'Kelly', 'jk0827@gmail.com', true),
 ('Ellie', 'Kelly', 'ellie.baldwin.kelly@gmail.com', true);
